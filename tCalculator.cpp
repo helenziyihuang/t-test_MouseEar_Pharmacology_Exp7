19f064e5e5e0fbed0c0b0466496c5651f026bb35
Ziyi Huang <ziyihuang@ucsb.edu>
2019-11-19 21:10:47 -0800

    create analysis of the treatment group

--- tCalculator.cpp
+++ tCalculator.cpp
@@ -66,6 +66,59 @@ int main(){
     cout << "Variance:              " << fixed << setprecision(3) << ctrlVariance << endl;
     cout << "Standard Deviation:    " << fixed << setprecision(3) << ctrlSD << endl;
     cout << "Standard Error:        " << fixed << setprecision(3) << ctrlSE << endl;
+    cout << endl;
+    
+    
+    // Treatment Group -------------------------------------------
+    
+    //Treatment Variables
+    int trNum;
+    double trSum = 0;
+    double trVariance = 0;
+    double trMean;
+    double trSD;
+    double trSE;
+    
+    cout << "Enter the number of individuals in your TREATMENT group: ";
+    cin >> trNum;
+    cout << endl;
+    
+    //Create an array to hold the value
+    double trV[trNum];
+    
+    //Get raw data
+    cout << "Enter the edema value (in mg) and press enter for" << endl;
+    for(int i=0; i<trNum; i++){
+        cout << "Individual " << i+1 << ": ";
+        cin >> trV[i];
+        
+        trSum += trV[i];
+    }
+    cout << endl;
+    
+    //Calculate Mean
+    trMean = trSum / trNum;
+    
+    //Calculate Variance
+    for(int i=0; i<trNum; i++){
+        trVariance += pow((trV[i] - trMean), 2);
+    }
+    trVariance /= (trNum - 1);
+    
+    //Calculate Standard Deviation
+    trSD = sqrt(trVariance);
+    
+    //Calculate Standard Error
+    trSE = sqrt(trVariance/trNum);
+    
+    //Display Control Calculation Results
+    cout << "----Treatment Group Analysis----" << endl;
+    
+    cout << "Mean:                  " << fixed << setprecision(3) << trMean << endl;
+    cout << "Variance:              " << fixed << setprecision(3) << trVariance << endl;
+    cout << "Standard Deviation:    " << fixed << setprecision(3) << trSD << endl;
+    cout << "Standard Error:        " << fixed << setprecision(3) << trSE << endl;
+    cout << endl;
     
     
 };
