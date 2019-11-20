34bf3ff6937db60284ee00b1cb7b597181f50b71
Ziyi Huang <ziyihuang@ucsb.edu>
2019-11-19 18:44:21 -0800

    update README info

--- tCalculator.cpp
+++ tCalculator.cpp
@@ -1,29 +1,13 @@
 /*
  Helen Huang, Nov. 19th, 2019
  Designed for MCDB 126AL Pharmacology Lab Experiment 7 Data Analysis
- 
- To run this program:
- Choice 1 (for Mac users):
- - Go to a terminal window
- - Go to the folder with this file by typing "cd" and the path
-    - Ex. cd desktop/tCalFolder/
-    - Ex. cd desktop/"MCDB 126AL"/"Exp 7"/"Data Analysis"/
- - Do a "make test" and you should see some outputs
- - Put in your data according to the instruction and you will be good
- 
- Choice 2:
- - Go to an online C++ compiler (Google it)
- - Copy the code in this tCalculator.cpp file into the online compiler
- - No need to copy the code from the Makefile
- - Press "Run"
- **/
+**/
 
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 
 using namespace std;
-
 
 int main(){
     int ctrlNumIndi;
